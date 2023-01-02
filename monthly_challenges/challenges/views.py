dd8eb08cd452e15590bf52ae6969c24a5d727149
alchemistbg <alchemist_bg@abv.bg>
2023-01-02 20:35:35 +0100

    Add first views to the project

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
 # Create your views here.
+
+
+def index(request):
+	print(request)
+	return HttpResponse("This works!\nThis is challenges' index!")
+
+
+def january(request):
+	print(request)
+	return HttpResponse("This works! This is january's challenge!\nEat no meat!")
+
+
+def february(request):
+	print(request)
+	return HttpResponse("This works! This is february's challenge!\nLearn Django every day!")
+
+
+def monthly_challenge(request, month):
+	challenges = {
+		"january": "This works! This is january's challenge!\nEat no meat!",
+		"february": "This works! This is february's challenge!\nLearn Django every day!",
+	}
+	return HttpResponse(challenges[month])
