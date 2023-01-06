5792c47884ba7154997dba9edfccb499f1c4635b
alchemistbg <alchemist_bg@abv.bg>
2023-01-06 18:15:02 +0100

    Register 'challenges' as app in thje django project

--- monthly_challenges/monthly_challenges/settings.py
+++ monthly_challenges/monthly_challenges/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'challenges', # This register challenges as app
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'monthly_challenges.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            # BASE_DIR / 'challenges' / 'templates' # This register this folder as app specific template folder
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
