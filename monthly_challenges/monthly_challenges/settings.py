b129e8f640c82488823cdc20c54c5ee54238fa0c
alchemistbg <alchemist_bg@abv.bg>
2023-01-07 20:58:40 +0100

    Make use of template inheritance
    All 'challenges' app pages are now using same parent template

--- monthly_challenges/monthly_challenges/settings.py
+++ monthly_challenges/monthly_challenges/settings.py
@@ -57,6 +57,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             # BASE_DIR / 'challenges' / 'templates' # This register this folder as app specific template folder
+            BASE_DIR / 'templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
