f727b53f0d70d16e1582b9301fb6daa76f3aa176
alchemistbg <alchemist_bg@abv.bg>
2023-01-08 09:38:41 +0100

    Make use of partials
    Create header and use it as a partial in index.html and in challenges.html

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name = 'index'),
     path("<int:month>", views.monthly_challenge_by_num),
     path("<str:month>", views.monthly_challenge_by_str,
          name="month_challenge_str"),
